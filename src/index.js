a48956b8387b91e061b9298dae9a3458dd974da5
lukehaf <Luke.D.Hafermann.25@Dartmouth.edu>
2021-06-14 09:27:49 -0700

    this has problems

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 // change require to es6 import style
 
 import './style.scss';
-
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.scss';
+import React, { Component } from 'react';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+import SearchBar from './components/search_bar';
+import youtubeSearch from './youtube-api';
+
+
+
+class App extends Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    videos: [],
+    selectedVideo: null,
+  };
+
+  youtubeSearch('pixar').then(videos => {
+    this.setState({
+      videos,
+      selectedVideo: videos[0],
+    });
+  });
+
+  render(); {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('main'));
-
-
